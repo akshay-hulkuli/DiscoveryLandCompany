7cb2974d60ee7bfd6f413e36a5d689197ebdeb64
Akshaya HP <akshayhulkuli@gmail.com>
2021-10-29 09:57:27 +0530

    ADDED: js file that provides navigation bar

---
+++ js/header.js
@@ -0,0 +1,19 @@
+document.querySelector('#nav-bar').innerHTML = `
+    <div class="nav-bar">
+        <div class="logo">
+            <img src="../assets/logo.png" alt="">
+            <div class="logo-name">
+                <span class="logo-text1">Discovery </span>
+                <span class="logo-text2">land company</span> 
+            </div>
+        </div>
+        <div class="options">
+            <span>About DLC</span>
+            <span>Our Worlds</span>
+            <span>Experiences</span>
+            <span>Gallery</span>
+            <span>Press</span>
+            <img src="../assets/options.png" alt="">
+        </div>
+    </div>
+`;
