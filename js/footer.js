29a370485aba2fed1c3d626ad786011f643dcea4
Akshaya HP <akshayhulkuli@gmail.com>
2021-10-29 09:57:53 +0530

    ADDED: js file that provides footer

---
+++ js/footer.js
@@ -0,0 +1,32 @@
+document.querySelector('#footer').innerHTML = `
+    <div class="footer">
+        <div class="footer-top">
+            <div><img src="../assets/logo.png"></div>
+            <div class="footer-data">
+                <div class="footer-content">
+                    <p style="color:hsla(0,0%,100%,.75);">GET IN TOUCH</p>
+                    <p>(480)634-5200</p>
+                    <p>mail@discoverylandco.com</p>
+                </div>
+                <div class="footer-content">
+                    <p>About DLC</p>
+                    <p>Our Worlds</p>
+                    <p>Experiences</p>
+                    <p>Gallery</p>
+                    <p>Press</p>
+                </div>
+                <div class="footer-content">
+                    <p>Magazine</p>
+                    <p>Careers</p>
+                    <p>Foundation</p>
+                    <p>Company</p>
+                    <p>Contact</p>
+                    <p>Privacy</p>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <span>@ DiscoveryLand Company. All rights reserved</span>
+        </div>
+    </div>
+`;
